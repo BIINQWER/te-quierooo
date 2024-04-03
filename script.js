7e234862bb9b0fe911a30976b9589d3828e0e24b
BIINQWER <165875885+BIINQWER@users.noreply.github.com>
2024-04-03 16:05:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mensajeDiv = document.getElementById("mensaje");
+  
+  // Mostrar mensaje "Te Quiero"
   mensajeDiv.textContent = "Te Quiero";
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  var mensajeDiv = document.getElementById("mensaje");
+  
+  // Titilar el mensaje
   var interval = setInterval(function () {
     if (mensajeDiv.style.visibility === "hidden") {
       mensajeDiv.style.visibility = "visible";
@@ -12,11 +12,8 @@ document.addEventListener("DOMContentLoaded", function () {
       mensajeDiv.style.visibility = "hidden";
     }
   }, 500); // Cambia este valor para ajustar la velocidad del titileo
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  var mensajeDiv = document.getElementById("mensaje");
-
+  
+  // Mostrar mensaje en diferentes idiomas
   var mensajes = [
     "Te Quiero", // Español
     "I Love You", // Inglés
@@ -34,13 +31,11 @@ document.addEventListener("DOMContentLoaded", function () {
     "Nakupenda", // Swahili
     "Jag älskar dig", // Sueco
   ];
-
+  
   var mensajeIndex = Math.floor(Math.random() * mensajes.length);
-
   mensajeDiv.textContent = mensajes[mensajeIndex];
-});
-
-document.addEventListener("DOMContentLoaded", function () {
+  
+  // Mostrar y ocultar menú de hamburguesa
   var menuToggle = document.getElementById("menu-toggle");
   var menu = document.getElementById("menu");
 
@@ -50,5 +45,19 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       menu.style.display = "block";
     }
+  });
+  
+  // Mostrar y ocultar submenú
+  var submenuToggle = document.querySelectorAll(".submenu-toggle");
+
+  submenuToggle.forEach(function (item) {
+    item.addEventListener("click", function () {
+      var submenu = this.querySelector(".submenu");
+      if (submenu.style.display === "block") {
+        submenu.style.display = "none";
+      } else {
+        submenu.style.display = "block";
+      }
+    });
   });
 });
